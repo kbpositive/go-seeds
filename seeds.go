f8a80ef8ae0e6f2e5c7cee2745bf709d5da755e2
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-07-06 19:09:25 -0500

    created update, and render functions

--- seeds.go
+++ seeds.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -26,9 +30,9 @@ func update(grid map[string]int) map[string]int {
 	}
 
 	var memo = make(map[string]int)
-	for key, _ := range grid {
+	for key := range grid {
 		var stack = []string{key}
-		for stack != nil {
+		for len(stack) > 0 {
 			cur := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			if _, found := memo[cur]; found != true {
@@ -63,9 +67,10 @@ func update(grid map[string]int) map[string]int {
 				}
 
 				// game rules B2/S
-				if _, found := grid[cur]; found != true && len(children) == 2 {
+				_, found := grid[cur]
+				if found != true && len(children) == 2 {
 					decoy[cur] = 1
-				} else if _, found := decoy[cur]; found {
+				} else if found {
 					delete(decoy, cur)
 				}
 
@@ -76,10 +81,71 @@ func update(grid map[string]int) map[string]int {
 	return decoy
 }
 
-// render grid function
-// block cellular automaton
+// render grid
+func render(grid map[string]int, frames int, dim int) {
+	var palette = []color.Color{
+		color.RGBA{0x00, 0x00, 0x00, 0xff}, color.RGBA{0x00, 0x00, 0xff, 0xff},
+		color.RGBA{0x00, 0xff, 0x00, 0xff}, color.RGBA{0x00, 0xff, 0xff, 0xff},
+		color.RGBA{0xff, 0x00, 0x00, 0xff}, color.RGBA{0xff, 0x00, 0xff, 0xff},
+		color.RGBA{0xff, 0xff, 0x00, 0xff}, color.RGBA{0xff, 0xff, 0xff, 0xff},
+	}
+	var images []*image.Paletted
+	var delays []int
+
+	for step := 0; step < frames; step++ {
+		img := image.NewPaletted(image.Rect(0, 0, dim, dim), palette)
+		images = append(images, img)
+		delays = append(delays, 0)
+
+		for x := 0; x < dim; x++ {
+			for y := 0; y < dim; y++ {
+				if _, found := grid[strconv.Itoa(x)+","+strconv.Itoa(y)]; found {
+					img.Set(x, y, color.RGBA{255, 255, 255, 255})
+				}
+			}
+		}
+		grid = update(grid)
+	}
+
+	f, err := os.OpenFile("seeds_of_chaos.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	gif.EncodeAll(f, &gif.GIF{
+		Image: images,
+		Delay: delays,
+	})
+
+	return
+}
+
+// chreate chaos pattern
+func chaos(grid map[string]int, x int, y int) map[string]int {
+	grid[strconv.Itoa(y)+","+strconv.Itoa(x+1)] = 1
+	grid[strconv.Itoa(y+1)+","+strconv.Itoa(x+3)] = 1
+	grid[strconv.Itoa(y+2)+","+strconv.Itoa(x)] = 1
+	grid[strconv.Itoa(y+2)+","+strconv.Itoa(x+2)] = 1
+	grid[strconv.Itoa(y+2)+","+strconv.Itoa(x+4)] = 1
+	grid[strconv.Itoa(y+3)+","+strconv.Itoa(x)] = 1
+	grid[strconv.Itoa(y+3)+","+strconv.Itoa(x+1)] = 1
+	grid[strconv.Itoa(y+3)+","+strconv.Itoa(x+4)] = 1
+	grid[strconv.Itoa(y+4)+","+strconv.Itoa(x+4)] = 1
+	grid[strconv.Itoa(y+4)+","+strconv.Itoa(x+5)] = 1
+	grid[strconv.Itoa(y+5)+","+strconv.Itoa(x+1)] = 1
+	grid[strconv.Itoa(y+5)+","+strconv.Itoa(x+2)] = 1
+	grid[strconv.Itoa(y+5)+","+strconv.Itoa(x+3)] = 1
+	grid[strconv.Itoa(y+6)+","+strconv.Itoa(x+3)] = 1
+	return grid
+}
 
 func main() {
+	// create grid
+	var grid = make(map[string]int)
 
-	fmt.Println(moves)
+	// add chaos pattern
+	grid = chaos(grid, 125, 125)
+	render(grid, 250, 250)
+
 }
